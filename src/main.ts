85ecbd2f8b673abad8a76396af92ef064782a54a
AnnaLakshmi <annalakshmi772002@gmail.com>
2026-05-22 15:27:17 +0530

    Fixed the cors error

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   }))
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173','https://deja-print-hub-frontend.vercel.app'],
     credentials: true,
   })
 
